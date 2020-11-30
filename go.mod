c1f6b342aa154ae481e501386cda7a23c5ba8a71
Denis Junuzovic <denisj1688@gmail.com>
2020-11-30 16:57:56 +0100

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module humanmongo
+
+go 1.12
+
+require go.mongodb.org/mongo-driver v1.4.3
---
+++ humanmongo.go
@@ -0,0 +1,31 @@
+package humanmongo
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// HummanCollection wrapper around mongo driver collection
+type HummanCollection struct {
+	*mongo.Collection
+}
+
+// FindAll sets the slice of the given result param or returns an error. Internally it uses the cursor.All method to assign the results.
+// It also sets and tears down the context (as TODO)
+func (c *HummanCollection) FindAll(query interface{}, result *interface{}, opt *options.FindOptions) error {
+	ctx := context.TODO()
+	defer ctx.Done()
+
+	cur, err := c.Find(ctx, query, opt)
+	defer cur.Close(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	err = cur.All(ctx, result)
+
+	return nil
+}
