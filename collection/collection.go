0b7571f76d97043ffea04586a3d16fd20852e711
denis <denis.junuzovic@fleetster.net>
2020-11-30 20:43:26 +0100

    some adjustments to parms

--- collection/collection.go
+++ collection/collection.go
@@ -7,18 +7,18 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// HummanCollection wrapper around mongo driver collection
-type HummanCollection struct {
+// Collection wrapper around mongo driver collection
+type Collection struct {
 	*mongo.Collection
 }
 
 // FindAll sets the slice of the given result param or returns an error. Internally it uses the cursor.All method to assign the results.
 // It also sets and tears down the context (as TODO)
-func (c *HummanCollection) FindAll(query interface{}, result *interface{}, opt *options.FindOptions) error {
+func (c *Collection) FindAll(query interface{}, result *[]interface{}, opts ...*options.FindOptions) error {
 	ctx := context.TODO()
 	defer ctx.Done()
 
-	cur, err := c.Find(ctx, query, opt)
+	cur, err := c.Find(ctx, query, opts...)
 	defer cur.Close(ctx)
 
 	if err != nil {
